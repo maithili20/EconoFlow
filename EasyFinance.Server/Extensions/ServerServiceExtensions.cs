c9c3632600fca4284a0198d3725a8ffb033768a6
Tiago Silva <neiva_tiago@hotmail.com>
2024-01-20 11:47:00 +0000

    Add Swagger config to external Service Extension

---
+++ EasyFinance.Server/Extensions/ServerServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace EasyFinance.Server.Extensions
+{
+    public static class ServerServiceExtensions
+    {
+        public static IServiceCollection AddServerServiceExtensions(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "EasyFinance API",
+                    Description = "An ASP.NET Core Web API for managing your expenses",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
