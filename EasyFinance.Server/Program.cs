aaa6be0e341ff76458404a5d98d7ae410c553be1
Tiago Silva <neiva_tiago@hotmail.com>
2024-01-20 13:18:28 +0000

    Update Swagger service

--- EasyFinance.Server/Program.cs
+++ EasyFinance.Server/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddServerServiceExtensions();
+builder.Services.AddSwaggerServiceExtensions();
 
 var app = builder.Build();
 
