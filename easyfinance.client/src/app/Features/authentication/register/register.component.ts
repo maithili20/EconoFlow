08b6fd5aade45c6e4db9a100e1a53d1189187910
Felipe Soares <>
2024-10-01 23:14:40 +0200

    Fix: Regex validation on create account

--- easyfinance.client/src/app/Features/authentication/register/register.component.ts
+++ easyfinance.client/src/app/Features/authentication/register/register.component.ts
@@ -32,7 +32,7 @@ export class RegisterComponent implements OnInit{
   buildRegisterForm(){
     this.registerForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}")]),
+      password: new FormControl('', [Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*\-_?&])[A-Za-z\d@$!%\-_*?&]{8,}")]),
       confirmPassword: new FormControl('',[Validators.required])
     },{validators: passwordMatchValidator}); 
   }
