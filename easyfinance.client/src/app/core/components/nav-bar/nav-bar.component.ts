eb87ce4ff7c8d04e1c509ab49c686cd8ead8baba
Felipe Soares <5514694+FelipePSoares@users.noreply.github.com>
2025-02-09 19:19:46 +0100

    Fix: Added button to show the menu on mobile. (#341)
    Co-authored-by: Felipe Soares <>

--- easyfinance.client/src/app/core/components/nav-bar/nav-bar.component.ts
+++ easyfinance.client/src/app/core/components/nav-bar/nav-bar.component.ts
@@ -12,7 +12,6 @@ import { ProjectService } from '../../services/project.service';
     styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-  initials$: Observable<string>;
   firstName$: Observable<string>;
   lastName$: Observable<string>;
   defaultLink: string = '/projects';
@@ -20,7 +19,6 @@ export class NavBarComponent implements OnInit {
   constructor(public userService: UserService, private projectService: ProjectService) {
     this.firstName$ = userService.loggedUser$.pipe(map(user => user.firstName));
     this.lastName$ = userService.loggedUser$.pipe(map(user => user.lastName));
-    this.initials$ = userService.loggedUser$.pipe(map(user => user.firstName[0] + user.lastName[0]));
   }
 
   ngOnInit(): void {
