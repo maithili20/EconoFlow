2c0fa71ada4c41b3a71b06c1c66d67f68ba0424b
Maithili Kalkar <k.maithilia@gmail.com>
2025-04-09 12:00:25 -0400

    Feat/#369 divide the progress bar into 4 sections (#433)
    * Updated to display the spending status as a percentage of the monthly budget.
    
    * divide the progress bar into weekly sections

--- easyfinance.client/src/app/core/components/budget-bar/budget-bar.component.ts
+++ easyfinance.client/src/app/core/components/budget-bar/budget-bar.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CurrencyFormatPipe } from '../../utils/pipes/currency-format.pipe';
+import { CurrentDateComponent } from '../../../core/components/current-date/current-date.component';
 
 @Component({
   selector: 'app-budget-bar',
@@ -26,15 +27,18 @@ export class BudgetBarComponent {
   date!: Date | undefined;
   @Input()
   hideDecimals: boolean = false;
+  weekLines: number[] = [];
+  @Input()
+  typeMonthOrYear: string = 'month';
 
   getPercentageSpend(spend: number, budget: number): number {
     return budget === 0 ? 0 : spend * 100 / budget;
   }
 
   getClassBasedOnPercentage(percentage: number): string {
-    if (percentage <= 75) {
+    if (percentage <= this.getCurrPercentageOfMonth()) {
       return '';
-    } else if (percentage <= 100) {
+    } else if (percentage <= 100 && percentage > this.getCurrPercentageOfMonth()){
       return 'warning';
     }
 
@@ -42,9 +46,9 @@ export class BudgetBarComponent {
   }
 
   getTextBasedOnPercentage(percentage: number): string {
-    if (percentage <= 75) {
+    if(percentage <= this.getCurrPercentageOfMonth()){
       return '';
-    } else if (percentage <= 100) {
+    }else if(percentage <= 100 && percentage > this.getCurrPercentageOfMonth()){
       return 'RiskOverspend';
     }
 
@@ -52,12 +56,44 @@ export class BudgetBarComponent {
   }
 
   getClassToProgressBar(percentage: number): string {
-    if (percentage <= 75) {
+    if(percentage <= this.getCurrPercentageOfMonth()){
       return 'bg-info';
-    } else if (percentage <= 100) {
+    }else if(percentage <= 100 && percentage > this.getCurrPercentageOfMonth()){
       return 'bg-warning';
     }
 
     return 'bg-danger';
+  }
+  getCurrPercentageOfMonth():number{
+    var today = new Date();
+    return today ? (today.getDate() / new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()) * 100 : 0;
+  }
+  getWeeksInCurrentMonth(): number {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), now.getMonth(), 1);
+    const end = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    const daysInMonth = end.getDate();
+
+    // Week starts on Sunday (0)
+    const startDay = start.getDay();
+    const totalWeeks = Math.ceil((startDay + daysInMonth) / 7);
+
+    return totalWeeks;
+  }
+
+  ngOnInit(): void {
+  if (this.checkIfCurrentMonth()) {
+    const weeks = this.getWeeksInCurrentMonth();
+    this.weekLines = Array.from({ length: weeks - 1 }, (_, i) => ((i + 1) / weeks) * 100);
+    }
+  }
+
+  checkIfCurrentMonth(): boolean {
+    var today = new Date();
+    return this.typeMonthOrYear=="month" && today.getMonth()  == CurrentDateComponent.currentDate.getMonth() && today.getFullYear() == CurrentDateComponent.currentDate.getFullYear();
+  }
+
+  checktypeMonthOrYear(): string {
+    return this.typeMonthOrYear;
   }
 }
